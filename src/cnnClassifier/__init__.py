f14f303f636b002cfd1676a9a6d755b6390483a0
SudhirSude <sudesudhir@gmail.com>
2025-01-11 17:13:53 +0530

    utils added

---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,22 @@
+#Logging is the process of recording events that occur during the execution of a software application or system. 
+
+import os
+import sys
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("cnnClassifierLogger")
